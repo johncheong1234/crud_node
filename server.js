7487a54129e68ef02155a3247ce5d038b2736712
johncheong1234 <john.cheong.2019@smu.edu.sg>
2021-09-12 21:54:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.");
+//   });
 
 // simple route
 app.get("/", (req, res) => {
